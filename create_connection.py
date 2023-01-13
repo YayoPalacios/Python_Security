2fa74bd9e19fe62772a4473d02b5a186e2b1792a
Yael Palacios <yayette95@gmail.com>
2023-01-13 12:18:29 -0600

    added error handling and user input for host and port

--- create_connection.py
+++ create_connection.py
@@ -1,13 +1,27 @@
+#!/usr/bin/env python3
+
 import socket
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = 'example.com'
-    port = 5000
-    s.settimeout(10) 
-    result = s.connect_ex((host, port))
-    print('The result is {}'.format(result))
-    s.close()
+    host = input("Enter hostname: ")
+    port = input("Enter port: ")
+    try:
+        port = int(port)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(10) 
+        result = s.connect_ex((host, port))
+        if result == 0:
+            print("Connection successful")
+        else:
+            print(f"Connection failed with error {result}")
+    except socket.timeout:
+        print("Error: Connection timed out")
+    except socket.gaierror:
+        print("Error: Invalid hostname or address")
+    except ValueError:
+        print("Error: Invalid port")
+    finally:
+        s.close()
 
 if __name__ == '__main__':
     main()
